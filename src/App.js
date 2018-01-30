9615a183d7bd5ebd6898943afeb6c02426caf9b4
zomars <zomars@me.com>
2018-01-30 12:29:08 -0700

    Semicolons must die

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import logo from './logo.svg'
 
 class App extends Component {
   render() {
@@ -13,8 +13,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
