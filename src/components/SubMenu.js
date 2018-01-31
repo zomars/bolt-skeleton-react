43f303a8f576fb4f54ccf90851b8e1242964220d
zomars <zomars@me.com>
2018-01-30 23:21:02 -0700

    Indenting

--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -5,17 +5,18 @@ const SubMenu = (props) => {
   const items = props.items || []
 
   return (
-  <ul className={`menu ${props.class || ''}`}>
-    {items.map((item, index) =>
-      <SubMenuItem
-        key={index}
-        item={item}
-        loop={index}
-        extraclass={props.extraclass}
-        withsubmenus={props.withsubmenus || true}
-      />
-    )}
-  </ul>
-)}
+    <ul className={`menu ${props.class || ''}`}>
+      {items.map((item, index) =>
+        <SubMenuItem
+          key={index}
+          item={item}
+          loop={index}
+          extraclass={props.extraclass}
+          withsubmenus={props.withsubmenus || true}
+        />
+      )}
+    </ul>
+  )
+}
 
 export default SubMenu
